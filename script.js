acfdd17e522bfca66d4995cac845bd92b78782cd
arabi <m.aneddame01@gmail.com>
2024-07-10 16:43:30 +0100

    .

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+const  grandparent = document.querySelector('.grandparent')
+
+// grandparent.forEach(changeBackground)
+
+changeBackground(grandparent)
+
+
+
+
+function changeBackground(element) {
+    element.style.backgroundColor = 'yellow'
+}
